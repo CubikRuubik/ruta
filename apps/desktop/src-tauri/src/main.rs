2402b88fdfc247aaf07fb133a6eecc41d37c5e47
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2025-11-03 21:57:35 +0200

    Update main.rs

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -6,7 +6,7 @@ use std::process::Command;
 
 fn main() {
     #[cfg(target_os = "macos")]
-    let current_path: PathBuf = {
+    let current_p: PathBuf = {
         let mut p = std::env::current_exe().expect("failed to get current exe path");
         p.pop();
         p.pop();
@@ -15,7 +15,7 @@ fn main() {
     };
 
     #[cfg(target_os = "windows")]
-    let current_path: PathBuf = {
+    let current_p: PathBuf = {
         let mut p = std::env::current_exe().expect("failed to get current exe path");
         p.pop(); // target/release/
         p.push("indexer");
@@ -23,7 +23,7 @@ fn main() {
     };
 
     std::thread::spawn(move || {
-        Command::new(current_path)
+        Command::new(current_p)
             .env("DATABASE_URL", "postgres://postgres:password@127.0.0.1:5432/indexer_db")
             .env("CONTRACT_ADDRESSES", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,0xdAC17F958D2ee523a2206206994597C13D831ec7")
             .spawn()
